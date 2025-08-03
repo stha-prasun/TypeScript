85d0e361f309a94df53c94f50270e49632045c9a
Prasun Shrestha <shresthaprasun88@gmail.com>
2025-08-03 12:52:35 +0545

    Functions is ts

---
+++ Basics/functions.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function addTwo(num) {
+    return num + 2;
+}
+addTwo(2);
+function signUpUser(name, email, age) {
+    console.log(name, email, age);
+}
+signUpUser("test", "email", 19);
---
+++ Basics/functions.ts
@@ -0,0 +1,13 @@
+function addTwo(num: number) {
+  return num + 2;
+}
+
+addTwo(2);
+
+function signUpUser(name: string, email: string, age: number) {
+  console.log(name, email, age);
+}
+
+signUpUser("test", "email", 19);
+
+export {};
