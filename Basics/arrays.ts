d6de91ce6082685a36a3b51b8eefb7f6aaad9e1d
Prasun Shrestha <shresthaprasun88@gmail.com>
2025-08-08 10:20:04 +0545

    Array Basics

---
+++ Basics/arrays.ts
@@ -0,0 +1,21 @@
+const superHeros: string[] = []; //The array should be of a type of string array
+
+const powerLevel: Array<number> = []; //Another syntax of array
+
+superHeros.push("Ironman");
+
+powerLevel.push(6);
+
+//An array of type User
+type User = {
+  name: string;
+  email: string;
+};
+
+const allUsers: User[] = [];
+
+//2D Array
+const MLModels: number[][] = [
+  [255, 255, 255],
+  [255, 255, 255],
+];
