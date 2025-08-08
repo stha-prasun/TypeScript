0184eff3117538dff6577737f839b90c581cbd2e
Prasun Shrestha <shresthaprasun88@gmail.com>
2025-08-08 09:28:43 +0545

    type aliases

---
+++ Basics/Type_Aliases.ts
@@ -0,0 +1,14 @@
+type User = {
+  name: string;
+  email: string;
+  isActive: boolean;
+};
+
+function createUser(user: User): User {
+  //says that the new user must be a type of User
+  return { name: "", email: "", isActive: true };
+}
+
+createUser({ name: "", email: "", isActive: true });
+
+export {};
